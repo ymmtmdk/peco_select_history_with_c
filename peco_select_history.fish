82a664cf7d0c1a5538e32b98c5c906351b2b657c
ymmtmdk <ymmtmdk@gmail.com>
2017-04-04 21:24:49 +0900

    first commit

--- peco_select_history.fish
+++ peco_select_history.fish
@@ -1,16 +1,3 @@
-function peco_select_history
-  if test (count $argv) -gt 0
-    set flags --query "$argv"
-  end
-
-  history merge
-  history --show-time='%y-%m-%d %H:%M> '| peco --layout=bottom-up $flags | sed -e "s/.\{16\}//"| read foo
-
-  if [ $foo ]
-    commandline $foo
-  end
-end
-
 function peco_select_history_with_c
   if test (count $argv) -gt 0
     set flags --query "$argv"
